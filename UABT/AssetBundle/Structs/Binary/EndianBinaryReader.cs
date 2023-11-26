56c6fc09e374778033898eadb8f77c1f49b4891f
Kemal Setya Adhi <30566970+neon-nyan@users.noreply.github.com>
2023-11-26 18:20:16 +0700

    Use unsafe methods for reading numbers and strings

--- UABT/AssetBundle/Structs/Binary/EndianBinaryReader.cs
+++ UABT/AssetBundle/Structs/Binary/EndianBinaryReader.cs
@@ -1,6 +1,8 @@
-﻿using Hi3Helper.Data;
-using System;
+﻿using System;
+using System.Buffers;
+using System.Buffers.Binary;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Hi3Helper.UABT.Binary
@@ -8,12 +10,6 @@ namespace Hi3Helper.UABT.Binary
     public class EndianBinaryReader : BinaryReader
     {
         public EndianType endian;
-
-        private byte[] a16 = new byte[2];
-
-        private byte[] a32 = new byte[4];
-
-        private byte[] a64 = new byte[8];
 
         public long Position
         {
@@ -35,131 +31,122 @@ namespace Hi3Helper.UABT.Binary
 
         public override short ReadInt16()
         {
-            if (endian == EndianType.BigEndian)
-            {
-                a16 = ReadBytes(2);
-                Array.Reverse(a16);
-                return BitConverter.ToInt16(a16, 0);
-            }
-            return base.ReadInt16();
+            Span<byte> buffer = stackalloc byte[2];
+            base.BaseStream.Read(buffer);
+
+            return endian == EndianType.BigEndian ? BinaryPrimitives.ReadInt16BigEndian(buffer)
+                : BinaryPrimitives.ReadInt16LittleEndian(buffer);
         }
 
         public override int ReadInt32()
         {
-            if (endian == EndianType.BigEndian)
-            {
-                a32 = ReadBytes(4);
-                Array.Reverse(a32);
-                return BitConverter.ToInt32(a32, 0);
-            }
-            return base.ReadInt32();
+            Span<byte> buffer = stackalloc byte[4];
+            base.BaseStream.Read(buffer);
+
+            return endian == EndianType.BigEndian ? BinaryPrimitives.ReadInt32BigEndian(buffer)
+                : BinaryPrimitives.ReadInt32LittleEndian(buffer);
         }
 
         public override long ReadInt64()
         {
-            if (endian == EndianType.BigEndian)
-            {
-                a64 = ReadBytes(8);
-                Array.Reverse(a64);
-                return BitConverter.ToInt64(a64, 0);
-            }
-            return base.ReadInt64();
+            Span<byte> buffer = stackalloc byte[8];
+            base.BaseStream.Read(buffer);
+
+            return endian == EndianType.BigEndian ? BinaryPrimitives.ReadInt64BigEndian(buffer)
+                : BinaryPrimitives.ReadInt64LittleEndian(buffer);
+        }
+
+        public Int128 ReadInt128()
+        {
+            Span<byte> buffer = stackalloc byte[16];
+            base.BaseStream.Read(buffer);
+
+            return endian == EndianType.BigEndian ? BinaryPrimitives.ReadInt128BigEndian(buffer)
+                : BinaryPrimitives.ReadInt128LittleEndian(buffer);
         }
 
         public override ushort ReadUInt16()
         {
-            if (endian == EndianType.BigEndian)
-            {
-                a16 = ReadBytes(2);
-                Array.Reverse(a16);
-                return BitConverter.ToUInt16(a16, 0);
-            }
-            return base.ReadUInt16();
+            Span<byte> buffer = stackalloc byte[2];
+            base.BaseStream.Read(buffer);
+
+            return endian == EndianType.BigEndian ? BinaryPrimitives.ReadUInt16BigEndian(buffer)
+                : BinaryPrimitives.ReadUInt16LittleEndian(buffer);
         }
 
         public override uint ReadUInt32()
         {
-            if (endian == EndianType.BigEndian)
-            {
-                a32 = ReadBytes(4);
-                Array.Reverse(a32);
-                return BitConverter.ToUInt32(a32, 0);
-            }
-            return base.ReadUInt32();
+            Span<byte> buffer = stackalloc byte[4];
+            base.BaseStream.Read(buffer);
+
+            return endian == EndianType.BigEndian ? BinaryPrimitives.ReadUInt32BigEndian(buffer)
+                : BinaryPrimitives.ReadUInt32LittleEndian(buffer);
         }
-
-        private ulong ToTarget(int tagBit)
-        {
-            byte code = base.ReadByte();
-            int offsetRead = 0;
-            ulong value = code & (((ulong)1 << (7 - tagBit)) - 1);
-
-            if ((code & (1 << (7 - tagBit))) != 0)
-            {
-                do
-                {
-                    if ((value >> (sizeof(ulong) * 8 - 7)) != 0) return 0;
-                    code = base.ReadByte();
-                    value = (value << 7) | (code & (((ulong)1 << 7) - 1));
-                    offsetRead++;
-                }
-                while ((code & (1 << 7)) != 0);
-            }
-            return value;
-        }
-
-        public ulong ReadUInt64VarInt() => ToTarget(0);
 
         public override ulong ReadUInt64()
         {
-            if (endian == EndianType.BigEndian)
-            {
-                a64 = ReadBytes(8);
-                Array.Reverse(a64);
-                return BitConverter.ToUInt64(a64, 0);
-            }
-            return base.ReadUInt64();
+            Span<byte> buffer = stackalloc byte[8];
+            base.BaseStream.Read(buffer);
+
+            return endian == EndianType.BigEndian ? BinaryPrimitives.ReadUInt64BigEndian(buffer)
+                : BinaryPrimitives.ReadUInt64LittleEndian(buffer);
+        }
+
+        public UInt128 ReadUInt128()
+        {
+            Span<byte> buffer = stackalloc byte[16];
+            base.BaseStream.Read(buffer);
+
+            return endian == EndianType.BigEndian ? BinaryPrimitives.ReadUInt128BigEndian(buffer)
+                : BinaryPrimitives.ReadUInt128LittleEndian(buffer);
         }
 
         public override float ReadSingle()
         {
-            if (endian == EndianType.BigEndian)
-            {
-                a32 = ReadBytes(4);
-                Array.Reverse(a32);
-                return BitConverter.ToSingle(a32, 0);
-            }
-            return base.ReadSingle();
+            Span<byte> buffer = stackalloc byte[4];
+            base.BaseStream.Read(buffer);
+
+            return endian == EndianType.BigEndian ? BinaryPrimitives.ReadSingleBigEndian(buffer)
+                : BinaryPrimitives.ReadSingleLittleEndian(buffer);
         }
 
         public override double ReadDouble()
         {
-            if (endian == EndianType.BigEndian)
-            {
-                a64 = ReadBytes(8);
-                Array.Reverse(a64);
-                return BitConverter.ToDouble(a64, 0);
-            }
-            return base.ReadDouble();
+            Span<byte> buffer = stackalloc byte[8];
+            base.BaseStream.Read(buffer);
+
+            return endian == EndianType.BigEndian ? BinaryPrimitives.ReadDoubleBigEndian(buffer)
+                : BinaryPrimitives.ReadDoubleLittleEndian(buffer);
         }
 
         public string ReadString8BitLength()
         {
-            byte count = ReadByte();
-            ReadOnlySpan<byte> strSpan = ReadBytes(count);
-            return Encoding.UTF8.GetString(strSpan);
+            sbyte len = (sbyte)base.BaseStream.ReadByte();
+            return ReadStringFromLen(len);
         }
 
         public override string ReadString()
         {
-            a16 = ReadBytes(2);
-            if (endian == EndianType.BigEndian)
+            ushort len = ReadUInt16();
+            return ReadStringFromLen(len);
+        }
+
+        private string ReadStringFromLen(int len)
+        {
+            byte[] buffer = ArrayPool<byte>.Shared.Rent(len);
+
+            string returnStr;
+            base.BaseStream.Read(buffer, 0, len);
+            unsafe
             {
-                Array.Reverse(a16);
+                fixed (sbyte* bufferByte = MemoryMarshal.Cast<byte, sbyte>(buffer))
+                {
+                    returnStr = new string(bufferByte, 0, len);
+                }
             }
-            ushort count = BitConverter.ToUInt16(a16);
-            ReadOnlySpan<byte> strSpan = ReadBytes(count);
-            return Encoding.UTF8.GetString(strSpan);
+
+            ArrayPool<byte>.Shared.Return(buffer);
+            return returnStr;
         }
     }
 }
